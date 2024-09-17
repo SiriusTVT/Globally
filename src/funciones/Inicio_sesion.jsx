d066fbaf9700de677d40b175787517d64b25f3d5
Juan David Troncoso <juandavid.troncoso@gmail.com>
2024-09-17 06:00:51 +0200

    Add login functionality and page

--- src/funciones/Inicio_sesion.jsx
+++ src/funciones/Inicio_sesion.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Inicio_sesion()
-{
+function Inicio_sesion() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleInputChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Aquí puedes hacer lo que necesites con los datos del formulario
+        console.log(formData);
+    };
+
     return (
         <div>
-            <h1>Inicio de sesion</h1>
+            <div className="background-overlay"></div>
+
+            <div className="content">
+                <h1 className='title'>Inicio de Sesión</h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <p className='label'>Email: </p>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder='Email'
+                            required
+                            className='input-field'
+                            value={formData.email}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <p className='label'>Contraseña: </p>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder='Contraseña'
+                            required
+                            className='input-field'
+                            value={formData.password}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <button className='button' type='submit'>Iniciar Sesión</button>
+                </form>
+            </div>
         </div>
     );
 }
