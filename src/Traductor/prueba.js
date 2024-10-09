480df252f1e57ce5ba9959aeeb910412998b98be
Juan David Troncoso <juandavid.troncoso@gmail.com>
2024-10-09 22:37:18 +0200

    Update LibreTranslate subproject commit and add translation test code

--- src/Traductor/prueba.js
+++ src/Traductor/prueba.js
@@ -9,3 +9,16 @@ const res = await fetch("http://127.0.0.1:5000/translate", {
   });
   
   console.log(await res.json());
+
+
+  const lar = await fetch("http://127.0.0.1:5000/translate", {
+    method: "POST",
+    body: JSON.stringify({
+      q: "Hello",
+      source: "auto",
+      target: "en"
+    }),
+    headers: { "Content-Type": "application/json" }
+  });
+  
+  console.log(await lar.json());
