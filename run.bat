d918220a3906690a83bf5e7d1c2dfd428dddec57
Juan David Troncoso <juandavid.troncoso@gmail.com>
2024-10-19 23:50:46 +0200

    Update run.bat script to check for Python 3.8 installation and install if necessary. Also update comments and fix typos.

--- run.bat
+++ run.bat
@@ -1,16 +1,30 @@
 @echo off
+echo Verificando si Python 3.8 está instalado...
+
+:: Comprueba si Python 3.8 está instalado
+py -3.8 --version >nul 2>&1
+if errorlevel 1 (
+    echo Python 3.8 no está instalado. Descargando e instalando...
+    :: Descarga Python 3.8 si no está instalado
+    curl -o python-3.8.10.exe https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe
+    start /wait python-3.8.10.exe /quiet InstallAllUsers=1 PrependPath=1
+    echo Python 3.8 instalado correctamente.
+) else (
+    echo Python 3.8 ya está instalado, omitiendo instalación.
+)
+
 echo Instalando los requerimientos previos de LibreTranslate...
 py -3.8 -m pip install libretranslate
 
 cd src\\Traductor
 
-echo Verificando si ya esta descomprimido...
+echo Verificando si ya está descomprimido...
 :: Verifica si existe la carpeta descomprimida (ajusta el nombre de la carpeta si es diferente)
 if not exist LibreTranslate\ (
     echo Descomprimiendo archivo .rar...
     "C:\\Program Files\\WinRAR\\WinRAR.exe" x LibreTranslate.rar . -y
 ) else (
-    echo Los archivos ya han sido descomprimidos, omitiendo descompresion.
+    echo Los archivos ya han sido descomprimidos, omitiendo descompresión.
 )
 
 cd LibreTranslate
